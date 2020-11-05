a11c0f4b59938b0091627f6f848f32575bbbaf8f
theusOliveira01 <theusoliveira01@dcc.ufrj.br>
2020-11-05 18:19:03 -0300

    Alteçao da funçao exec_comando para executar comandos nao-built-in

--- shell.c
+++ shell.c
@@ -9,6 +9,8 @@
 
 #define clear() printf("\033[H\033[J") 
 
+#define NUM_COMANDOS 5
+
 void inicia_shell(){
     
     clear();
@@ -32,13 +34,82 @@ char *get_input(){
     return NULL;
 }
 
+void exec_comando(char *comando){
+
+    char *arg_comando;
+    
+    // Comando que o usuário entrou
+    comando = strtok(comando, " ");
+
+    // Argumento do comando.
+    arg_comando = strtok(NULL, " ");
+
+    char *argumentos_shell[3] = {comando, arg_comando, NULL};
+
+    // Caso não seja um comando built-in executamos esse bloco
+    if(strcmp(comando, ".") || strcmp(comando, "/") == 0){
+        pid_t pid = fork();
+     
+        if(pid == -1){
+            puts("Falha ao executar um novo processo.");
+        }
+        //Processo filho sendo executado
+        else if(pid == 0){
+
+            if(execvp(argumentos_shell[0], argumentos_shell) < 0){
+                puts("Não foi possível executar o comando.");
+                return;
+            }
+
+        }else{
+            //O processo pai espera o processo filho terminar
+            wait(NULL);
+            return;
+        }
+    }
+    
+    //Lista de comandos built-in possíveis
+    char *comandos[NUM_COMANDOS] = {"quit", "fg", "bg", "jobs", "cd"};
+    
+    // Será o index do argumento chamado, SE esse argumento for válido.
+    int numero_agv = -1;    
+    
+    for (int i = 0; i < NUM_COMANDOS; i++)
+    {
+        if(strcmp(comando, comandos[i]) == 0){
+            numero_agv = i;
+            break;
+        }
+
+    }
+    
+    if(numero_agv = -1){
+        puts("Comando built-in não reconhecido.");
+        return;
+    }    
+        
+    switch (numero_agv)
+    {    
+    case 0:        
+        break;
+    
+    default:
+        break;
+    }
+}
+
+
+
 int main(){    
     
     inicia_shell();
 
-    if(get_input() != NULL){
-        
-    }
+    while(1){
+        char *comando = get_input();
+        if(comando != NULL){
+            exec_comando(comando);
+        }
+    }  
     
 
     exit(0);
